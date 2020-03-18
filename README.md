2a775470c3613f1cbea63038a3ddbf65b0ccefe7
xys20071111 <xys20071111@hotmail.com>
2020-03-18 11:40:01 +0800

    整理API

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ljlx-BigData
 抓取乐教乐学的数据
+## API  
+*?*换成参数
+|url|描述|
+|----|----| 
+|http://eduyun.ljlx.com/ljlx/app/lxapplist?pageNo=*?*&pageSize=*?*| 获取应用信息|
+|http://eduyun.ljlx.com/appdata/stat/map?siteId=*?*|获取区域使用数据|
+|http://eduyun.ljlx.com/appdata/stat/mapDynamic?top=*?*&siteId=*?*&inteval=60|获取区域用户动态|
+|http://eduyun.ljlx.com/appdata/app/toplist?appid=*?*&top=12&siteId=*?*|获取区域指定应用数据(返回数据随appid变化))|
+|http://eduyun.ljlx.com/ljlx/app/lxappstat|APP列表|
+|http://eduyun.ljlx.com/ljlx/app/appstat|APP列表|
+
+## siteId  
+|siteId|区域|
+|----|----|
+|d43f1ff7-ae36-4b1e-9af8-98a9cf027c22|全国|
+
+
+## appid  
+|appid|应用|
+|----|----|
+|1033|才艺秀|
+|1093|博客|
--- UserActivity.py
+++ UserActivity.py
@@ -3,12 +3,14 @@ from threading import Thread
 import socket
 class getUserActivity(Thread):
     def __init__(self,siteID):
-        self._siteID = siteID
+        self.__siteID = siteID
+        self.__rpc = socket.scoket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__rpc.connect(('127.0.0.1',5624))
         super().__init__()
     def run(self):
-        res = get('http://eduyun.ljlx.com/appdata/stat/mapDynamic?top=200&siteId=' + self._siteID)
+        res = get('http://eduyun.ljlx.com/appdata/stat/mapDynamic?top=200&siteId=' + self.__siteID)
         data = res.text
-        self.rpc.sendall(data.encode('utf-8'))
+        self.__rpc.sendall(data.encode("utf-8"))
 if __name__ == "__main__":
     getUserActivity('d43f1ff7-ae36-4b1e-9af8-98a9cf027c22').run()
         
